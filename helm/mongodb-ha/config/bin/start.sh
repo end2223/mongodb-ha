1cd6d1eca11a4a65990aff63fd5d8a0f2476bd9b
Cao Ngoc Son <caongocson2122@gmail.com>
2022-03-28 00:21:28 +0700

    ok, but not perfect

--- helm/mongodb-ha/config/bin/start.sh
+++ helm/mongodb-ha/config/bin/start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 /usr/local/bin/docker-entrypoint.sh mongod --replSet "$REPLICA_CLUSTER" &> /dev/null &
 sleep 10
+cp -r /data /data-777
 if [[ "$POD_NAME" -eq "mongodb-stateful-set-0" ]]
 then
     sleep 120
-    /data/etc/gen-config-replica.sh
-    mongo < /data/etc/create-replica.js
-    mongo < /data/etc/setup-account.js
+    /data-777/etc/gen-config-replica.sh
+    mongo < /data-777/etc/create-replica.js
+    mongo < /data/-777etc/setup-account.js
     mongo -u $REPLICA_USER -p $REPLICA_PASSWORD --eval "rs.status()" --authenticationDatabase $DATABASE_NAME
 fi
+
+tail -f /dev/null
--- helm/mongodb-ha/config/etc/gen-config-replica.sh
+++ helm/mongodb-ha/config/etc/gen-config-replica.sh
@@ -11,8 +11,8 @@ replica="{\"_id\": \"$REPLICA_CLUSTER\", \"members\": [${replica%,*}]}"
 sed -i "s/__CONFIG/$replica/" create-replica.js
 
 # update create account admin, cluster
-sed -i "s/__database_name/$DATABASE_NAME/g" setup-account.js
-sed -i "s/__admin_user/$ADMIN_USER/g" setup-account.js
-sed -i "s/__admin_password/$ADMIN_PASSWORD/g" setup-account.js
-sed -i "s/__replica_user/$REPLICA_USER/g" setup-account.js
-sed -i "s/__replica_password/$REPLICA_PASSWORD/g" setup-account.js
+sed -i "s/__database_name/$DATABASE_NAME/g" /data-777/etc/setup-account.js
+sed -i "s/__admin_user/$ADMIN_USER/g" /data-777/etc/setup-account.js
+sed -i "s/__admin_password/$ADMIN_PASSWORD/g" /data-777/etc/setup-account.js
+sed -i "s/__replica_user/$REPLICA_USER/g" /data-777/etc/setup-account.js
+sed -i "s/__replica_password/$REPLICA_PASSWORD/g" /data-777/etc/setup-account.js
