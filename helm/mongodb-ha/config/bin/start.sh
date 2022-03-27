0f55376e43e2dbe3808d1f6636e83cb0ecf33d22
Cao Ngoc Son <caongocson2122@gmail.com>
2022-03-27 15:49:14 +0700

    deed

--- helm/mongodb-ha/config/bin/start.sh
+++ helm/mongodb-ha/config/bin/start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-sleep 30
-/data/etc/gen-config-replica.sh
-mongo < /data/etc/create-replica.js
-mongo < /data/etc/setup-account.js
-mongo -u $REPLICA_USER -p $REPLICA_PASSWORD --eval "rs.status()" --authenticationDatabase $DATABASE_NAME
+/usr/local/bin/docker-entrypoint.sh mongod --replSet "$REPLICA_CLUSTER" &> /dev/null &
+sleep 10
+if [[ "$POD_NAME" -eq "mongodb-stateful-set-0" ]]
+then
+    sleep 120
+    /data/etc/gen-config-replica.sh
+    mongo < /data/etc/create-replica.js
+    mongo < /data/etc/setup-account.js
+    mongo -u $REPLICA_USER -p $REPLICA_PASSWORD --eval "rs.status()" --authenticationDatabase $DATABASE_NAME
+fi
--- helm/mongodb-ha/config/etc/gen-config-replica.sh
+++ helm/mongodb-ha/config/etc/gen-config-replica.sh
@@ -3,7 +3,7 @@
 # generate config for replica
 replica=""
 for ((i=0; i<$NUMBER_REPLICA; i++)); do
-  replica="$replica{\"_id\": \"$i\", \"host\": \"mongodb-$i:27017\"}, "
+  replica="$replica{\"_id\": $i, \"host\": \"mongodb-stateful-set-$i.mongodb-service:27017\"}, "
 done;
 replica="{\"_id\": \"$REPLICA_CLUSTER\", \"members\": [${replica%,*}]}"
 
@@ -16,21 +16,3 @@ sed -i "s/__admin_user/$ADMIN_USER/g" setup-account.js
 sed -i "s/__admin_password/$ADMIN_PASSWORD/g" setup-account.js
 sed -i "s/__replica_user/$REPLICA_USER/g" setup-account.js
 sed -i "s/__replica_password/$REPLICA_PASSWORD/g" setup-account.js
-
-
-# admin = db.getSiblingDB("__database_name")
-# admin.createUser(
-#   {
-#     user: "__admin_user",
-#     pwd: "__admin_password",
-#     roles: [ { role: "userAdminAnyDatabase", db: "__database_name" } ]
-#   }
-# )// let's authenticate to create the other user
-# db.getSiblingDB("__database_name").auth("__admin_user", "__admin_password" )
-# db.getSiblingDB("__database_name").createUser(
-#   {
-#     "user" : "__replica_user",
-#     "pwd" : "__replica_password",
-#     roles: [ { "role" : "clusterAdmin", "__replica_user" : "__replica_password" } ]
-#   }
-# )
